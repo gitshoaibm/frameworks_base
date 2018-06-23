f99f4e9bec9d7ed26477487a48a641bd912988eb
Shoaib <shoaib.gcpdev@gmail.com>
2018-06-23 17:03:12 +0000

    Revert "pick 909cc2578fa9db7dbebbe431e7cd66433a804c5d[1/3] base: OmniSwitch integration bits"
    This reverts commit 365ba6ad10ffc980a0cd80fb37c0fe04b9bf56e4.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -260,7 +260,6 @@ import com.android.internal.policy.IShortcutService;
 import com.android.internal.policy.PhoneWindow;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.util.ScreenShapeHelper;
-import com.android.internal.util.omni.OmniSwitchConstants;
 import com.android.internal.utils.du.ActionHandler;
 import com.android.internal.utils.du.DUActionUtils;
 import com.android.internal.util.screwd.screwdUtils;
@@ -296,7 +295,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     static final String TAG = "WindowManager";
     static final boolean DEBUG = false;
     static final boolean localLOGV = false;
-    static boolean DEBUG_INPUT = false;
+    static final boolean DEBUG_INPUT = false;
     static final boolean DEBUG_KEYGUARD = false;
     static final boolean DEBUG_LAYOUT = false;
     static final boolean DEBUG_SPLASH_SCREEN = false;
@@ -398,9 +397,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             "com.android.systemui.screenshot.TakeScreenshotService";
     private static final String SYSUI_SCREENSHOT_ERROR_RECEIVER =
             "com.android.systemui.screenshot.ScreenshotServiceErrorReceiver";
-
-    // OmniSwitch
-    private boolean mOmniSwitchRecents;
 
     /**
      * Keyguard stuff
@@ -1166,7 +1162,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.VOLUME_ROCKER_WAKE), false, this,
-                    Settings.System.RECENTS_OMNI_SWITCH_ENABLED), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -2372,9 +2367,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mAccessibilityManager = (AccessibilityManager) context.getSystemService(
                 Context.ACCESSIBILITY_SERVICE);
 
-        boolean debugInputOverride = SystemProperties.getBoolean("debug.inputEvent", false);
-        DEBUG_INPUT = DEBUG_INPUT || debugInputOverride;
-
         // register for dock events
         IntentFilter filter = new IntentFilter();
         filter.addAction(UiModeManager.ACTION_ENTER_CAR_MODE);
@@ -2906,9 +2898,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mTorchActionMode = Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0,
                     UserHandle.USER_CURRENT);
-            mOmniSwitchRecents = Settings.System.getIntForUser(resolver,
-                    Settings.System.RECENTS_OMNI_SWITCH_ENABLED, 0,
-                    UserHandle.USER_CURRENT) == 1;
         }
 
         synchronized (mWindowManagerFuncs.getWindowManagerLock()) {
@@ -4126,9 +4115,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // Remember that home is pressed and handle special actions.
             if (repeatCount == 0) {
                 mHomePressed = true;
-                if (mOmniSwitchRecents) {
-                    OmniSwitchConstants.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
-                }
                 if (mHomeDoubleTapPending) {
                     mHomeDoubleTapPending = false;
                     mHandler.removeCallbacks(mHomeDoubleTapTimeoutRunnable);
@@ -4774,10 +4760,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void preloadRecentApps() {
-        if (mOmniSwitchRecents) {
-            OmniSwitchConstants.preloadOmniSwitchRecents(mContext, UserHandle.CURRENT);
-            return;
-        }
         mPreloadedRecentApps = true;
         StatusBarManagerInternal statusbar = getStatusBarManagerInternal();
         if (statusbar != null) {
@@ -4786,9 +4768,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void cancelPreloadRecentApps() {
-        if (mOmniSwitchRecents) {
-            return;
-        }
         if (mPreloadedRecentApps) {
             mPreloadedRecentApps = false;
             StatusBarManagerInternal statusbar = getStatusBarManagerInternal();
@@ -4799,29 +4778,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void toggleRecentApps() {
-        if (mOmniSwitchRecents) {
-            OmniSwitchConstants.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
-        } else {
-            mPreloadedRecentApps = false; // preloading no longer needs to be canceled
-            StatusBarManagerInternal statusbar = getStatusBarManagerInternal();
-            if (statusbar != null) {
-                statusbar.toggleRecentApps();
-            }
+        mPreloadedRecentApps = false; // preloading no longer needs to be canceled
+        StatusBarManagerInternal statusbar = getStatusBarManagerInternal();
+        if (statusbar != null) {
+            statusbar.toggleRecentApps();
         }
     }
 
     @Override
     public void showRecentApps(boolean fromHome) {
-        if (mOmniSwitchRecents) {
-            if (!fromHome) {
-                OmniSwitchConstants.restoreHomeStack(mContext, UserHandle.CURRENT);
-            } else {
-                OmniSwitchConstants.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
-            }
-        } else {
-            mHandler.removeMessages(MSG_DISPATCH_SHOW_RECENTS);
-            mHandler.obtainMessage(MSG_DISPATCH_SHOW_RECENTS, fromHome ? 1 : 0, 0).sendToTarget();
-        }
+        mHandler.removeMessages(MSG_DISPATCH_SHOW_RECENTS);
+        mHandler.obtainMessage(MSG_DISPATCH_SHOW_RECENTS, fromHome ? 1 : 0, 0).sendToTarget();
     }
 
     private void showRecentApps(boolean triggeredFromAltTab, boolean fromHome) {
